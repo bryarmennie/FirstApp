75f39770a1fb0c7d9aff4348e764e022182bb76f
1504170 <1504170@Y348SDP07.ad.dundeeandangus.ac.uk>
2016-11-24 10:44:17 +0000

    1.0 project

--- src/firstapp/Main.java
+++ src/firstapp/Main.java
@@ -16,8 +16,29 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // something
-        System.out.println("Hello World!");
+        String input = "Chanandler Bong";
+        String rot13 = rot13(input);
+        String normal = rot13(rot13);
+
+        System.out.println(input);
+        System.out.println(rot13);
+        System.out.println(normal);
     }
-    
+
+    public static String rot13(String value) {
+        char[] values = value.toCharArray();
+        for (int i = 0; i < values.length; i++) {
+            char letter = values[i];
+
+            if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')) {
+                if (letter > 'm' || letter > 'M') {
+                    letter -= 13;
+                } else {
+                    letter += 13;
+                }
+            }
+            values[i] = letter;
+        }
+        return new String(values);
+    }
 }

047b32550b270552c863719e861c05b97a56c85b
1504170 <1504170@Y348SDP07.ad.dundeeandangus.ac.uk>
2016-11-24 09:42:07 +0000

    1.0 project

--- src/firstapp/Main.java
+++ src/firstapp/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // make changes
+        System.out.println("Hello World!");
     }
     
 }
